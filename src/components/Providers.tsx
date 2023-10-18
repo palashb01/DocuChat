6e49a9e7fe5843942c086fcd0f593eb405b015f7
palashb01 <palash.baderia@outlook.com>
2023-10-18 10:53:50 +0530

    trpc: Fix the absolute URl.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,13 +4,14 @@ import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
+import {absoluteURl} from "@/lib/utils";
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: absoluteURl('/api/trpc')
         }),
       ],
     }),
